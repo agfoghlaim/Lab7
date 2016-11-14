15686c5199a88c6f3bb1b0fae2a3e2a0f5104102
Agfoghlaim <marieohara.gti@gmail.com>
2016-11-14 14:18:21 +0000

    Q2 part2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,3 +28,15 @@ app.controller('studentController', function($scope,studentAgeService){
 	var s4 = new Student('Fred', 'Athenry', new Date(1994, 07, 11));
 	$scope.students.push(s1,s2,s3,s4);
 });
+
+app.controller('GMITController', function($scope, studentAgeService){
+	$scope.GMIT = {
+		name: 'Galway-Mayo Institute of Technology', 
+		address:'Galway, Letterfrack, Mountbellew, Castlebar',
+		founded: new Date(1972,08,18),
+		age: studentAgeService.getAge(new Date(1972,08,18))
+
+		
+	
+	};
+});
